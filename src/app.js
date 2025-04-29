9b75bf606211e3253c3eaf440ff11212c97629b1
giok2154 <giovannycacua3@gmail.com>
2025-04-29 18:17:14 +0000

    entrego proyecto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
+
+  let pronombres = ["the", "our"];
+  let adjettivos = ["great", "big"];
+  let sustantivos = ["jogger", "racoon"];
+
+
+
+  let texhtml = ""
+
+  for (let p = 0; p < pronombres.length; p++) {
+    for (let a = 0; a < adjettivos.length; a++) {
+      for (let s = 0; s < sustantivos.length; s++) {
+        //let li = document. createElement ("li");
+
+        texhtml += `<li>${pronombres[p]}${adjettivos[a]}${sustantivos[s]}.com</li>`
+
+      }
+    }
+  }
+
+
+
+  let dominios = document.querySelector("#dominios")
+  dominios.innerHTML = texhtml
   console.log("Hello Rigo from the console!");
 };
