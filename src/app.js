16af48ba54790196175e4ab7c8b5598d5a507c60
giok2154 <giovannycacua3@gmail.com>
2025-05-06 18:00:40 +0000

    proyecto corregido

--- src/app.js
+++ src/app.js
@@ -1,35 +1,25 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
-  //write your code here
 
+window.onload = function () {
   let pronombres = ["the", "our"];
   let adjettivos = ["great", "big"];
   let sustantivos = ["jogger", "racoon"];
-
-
-
-  let texhtml = ""
+  let textHTML = ""
 
   for (let p = 0; p < pronombres.length; p++) {
     for (let a = 0; a < adjettivos.length; a++) {
       for (let s = 0; s < sustantivos.length; s++) {
-        //let li = document. createElement ("li");
-
+        // Let li = document. createElement ("li");
         texhtml += `<li>${pronombres[p]}${adjettivos[a]}${sustantivos[s]}.com</li>`
-
       }
     }
   }
 
-
-
   let dominios = document.querySelector("#dominios")
-  dominios.innerHTML = texhtml
+  dominios.innerHTML = textHTML
   console.log("Hello Rigo from the console!");
 };
